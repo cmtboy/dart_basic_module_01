ac4eefba0c724d97f6986a8f03a75826add54217
CmtBoy <alaminislam.dev@gmail.com>
2023-08-04 21:12:15 +0600

    extra comment removed

--- bin/dart_basic_module_01.dart
+++ bin/dart_basic_module_01.dart
@@ -1,12 +1,12 @@
 void main() {
-  var a = 23;
-  var b = 9;
+  int a = 23;
+  int b = 10;
 
-  var add = a + b;
-  var sub = a - b;
-  var mul = a * b;
-  var div = a / b;
-  var mod = a % b;
+  int add = a + b;
+  int sub = a - b;
+  int mul = a * b;
+  double div = a / b;
+  int mod = a % b;
 
   print('a=$a, b=$b');
   print('Addition: $a + $b = $add');
@@ -15,17 +15,3 @@ void main() {
   print('Division: $a / $b = $div');
   print('Modulus: $a % $b = $mod');
 }
-
-//OUTPUT:
-/* 
-
-a=23, b=9
-Addition: 23 + 9 = 32
-Subtraction: 23 - 9 = 14
-Multiplication: 23 * 9 = 207
-Division: 23 / 9 = 2.5555555555555554
-Modulus: 23 % 9 = 5
-
-Exited.
-
-*/
